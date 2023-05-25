d9a91c82f9d270941a746d8701b149cd728a1869
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-05-25 09:04:02 +0300

    Update contactform-process.php

--- contactform-process.php
+++ contactform-process.php
@@ -1,55 +1,51 @@
 <?php
 $errorMSG = "";
 
+// Validate and sanitize name input
 if (empty($_POST["name"])) {
-    $errorMSG = "Name is required ";
+    $errorMSG .= "Name is required. ";
 } else {
-    $name = $_POST["name"];
+    $name = filter_var($_POST["name"], FILTER_SANITIZE_STRING);
 }
 
+// Validate and sanitize email input
 if (empty($_POST["email"])) {
-    $errorMSG = "Email is required ";
+    $errorMSG .= "Email is required. ";
 } else {
-    $email = $_POST["email"];
-}
-
-if (empty($_POST["message"])) {
-    $errorMSG = "Message is required ";
-} else {
-    $message = $_POST["message"];
-}
-
-
-
-$EmailTo = "sales@digikyar.org";
-$Subject = "New message from SALES digikyar";
-
-// prepare email body text
-$Body = "";
-$Body .= "Name: ";
-$Body .= $name;
-$Body .= "\n";
-$Body .= "Email: ";
-$Body .= $email;
-$Body .= "\n";
-$Body .= "Message: ";
-$Body .= $message;
-$Body .= "\n";
-$Body .= "Terms: ";
-$Body .= $terms;
-$Body .= "\n";
-
-// send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
-
-// redirect to success page
-if ($success && $errorMSG == ""){
-   echo "success";
-}else{
-    if($errorMSG == ""){
-        echo "Something went wrong :(";
-    } else {
-        echo $errorMSG;
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errorMSG .= "Invalid email format. ";
     }
+}
+
+// Validate and sanitize message input
+if (empty($_POST["message"])) {
+    $errorMSG .= "Message is required. ";
+} else {
+    $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+}
+
+// Check if there are any errors
+if (empty($errorMSG)) {
+    $toEmail = "sales@digikyar.org";
+    $subject = "New message from SALES digikyar";
+
+    // Prepare email body text
+    $body = "Name: " . $name . "\n";
+    $body .= "Email: " . $email . "\n";
+    $body .= "Message: " . $message . "\n";
+
+    // Set additional email headers
+    $headers = "From: " . $email . "\r\n";
+    $headers .= "Reply-To: " . $email . "\r\n";
+
+    // Send the email
+    if (mail($toEmail, $subject, $body, $headers)) {
+        echo "success";
+    } else {
+        echo "Something went wrong while sending the email.";
+    }
+} else {
+    echo $errorMSG;
 }
 ?>
